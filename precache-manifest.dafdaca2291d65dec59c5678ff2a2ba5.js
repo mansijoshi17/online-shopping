91c04a8beb2b10713fdedfdd5a5409aed3acdea2
Mansi Joshi <mansijoshi1777@gmail.com>
2020-02-06 11:50:36 +0530

    Updates

---
+++ precache-manifest.dafdaca2291d65dec59c5678ff2a2ba5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193ada4b19dc85459764c289626038c2",
+    "url": "/online-shopping/index.html"
+  },
+  {
+    "revision": "01a85a3e23dc966de3d1",
+    "url": "/online-shopping/static/css/main.471fe122.chunk.css"
+  },
+  {
+    "revision": "948c24743aa4b3c49565",
+    "url": "/online-shopping/static/js/2.3add04c2.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/online-shopping/static/js/2.3add04c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01a85a3e23dc966de3d1",
+    "url": "/online-shopping/static/js/main.140d97e3.chunk.js"
+  },
+  {
+    "revision": "823f0ad9655f3da24e0f",
+    "url": "/online-shopping/static/js/runtime-main.e76195be.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/online-shopping/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/online-shopping/static/media/shopping-bag.30f07071.svg"
+  }
+]);
